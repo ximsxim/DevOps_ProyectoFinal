aff130b1ad34708d6a80e1fa13d50c6a15739157
Fernanda Ximena Garcia Salazar <xcate682@gmail.com>
2026-04-15 00:27:25 -0600

    Correccion: comillas en setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ echo "Actualizando el sistema..."
 sudo apt update -y
 echo "Instalando git, vim, docker y python3..."
 sudo apt install -y git vim docker.io python3 python3-pip
-echo "Instalacion completada."!"
+echo "Instalacion completada."
