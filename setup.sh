9282a73128f813e2ad609c2cb8c76a239104fe8d
Fernanda Ximena Garcia Salazar <xcate682@gmail.com>
2026-04-14 23:34:40 -0600

    Mejora: agregado set -e para robustez en setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 echo "Actualizando el sistema..."
 sudo apt update -y
 echo "Instalando git, vim, docker y python3..."
 sudo apt install -y git vim docker.io python3 python3-pip
-echo "¡Instalación completada a la perfección!"
+echo "Instalacion completada."!"
